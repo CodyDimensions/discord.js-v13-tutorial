4522c3889021cd735747fcd59b4f939f324a1f02
CodyDimensions <92368816+CodyDimensions@users.noreply.github.com>
2022-01-15 03:42:57 -0800

    Fixed not updated discord.js code and error

--- EP4 - Say Command/commands/utility/say.js
+++ EP4 - Say Command/commands/utility/say.js
@@ -4,17 +4,19 @@ module.exports = {
     run: async(client, message, args) => {
         let msg;
         let textchannel = message.mentions.channels.first() //find the channel you mention in the cmd
-        message.delete()
-        if(!message.member.permissions.has('MANAGE_MESSAGES')) { //need manager messages permission to prevent people use this cmd wrong
-            return message.reply('**You do not have permission to use this command.**') //like sending the msg to announcement channel...
+        
+        if(!message.member.permissions.has('MANAGE_MESSAGES')) { //need manage messages permission to prevent people use this cmd wrong
+            return message.reply({ content: '**You do not have permission to use this command.**' }) //like sending the msg to announcement channel...
         } else if(!args[0]) { // if you did not type what you wanna say, then the bot will return a message to you
-            return message.reply('**Please specify what you want to say!**')
+            return message.reply({ content: '**Please specify what you want to say!**'})
         }else if(textchannel) { //if you mention the channel you want to send the message
+        message.delete()
             msg = args.slice(1).join(' '); // for example: !say #general hello
-            textchannel.send(msg) //then it will send that message to the channel you mention
+            client.channels.cache.get(textchannel.id).send({ content: msg }) //then it will send that message to the channel you mention
         }else{
+          message.delete()
             msg = args.join(' '); //this code is for getting the message you want to send
-            message.channel.send(msg)//then send to your message channel
+            message.channel.send({ content: msg })//then send to your message channel
         }
     }
 }
